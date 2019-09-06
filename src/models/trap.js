f5712364cc77d1063f0e539124c1e803931fb59a
Duane Alcala <duane.y.alcala@gmail.com>
2019-09-07 01:55:44 +1200

    Created folder for constants and model for a tourist trap object

---
+++ src/models/trap.js
@@ -0,0 +1,14 @@
+class Trap {
+  constructor(id, userId, name, location, country, details, lat, long) {
+    this.id = id;
+    this.userId = userId;
+    this.name = name;
+    this.location = location;
+    this.country = country;
+    this.details = details;
+    this.lat = lat;
+    this.long = long;
+  }
+}
+
+export default Trap;
